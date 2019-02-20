86dde2b24e85de253fdfb446e8f8ef99342ffebd
PBOCER <przemek.bocer@gmail.com>
2019-02-20 09:37:36 +0100

    Initial commit

---
+++ src/main/java/gdgdrg/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package gdgdrg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
